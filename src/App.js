396f557b002f2ddd67e520db505d466806bd5bc4
Ian Wafula <neocorpse@proton.me>
2025-04-18 18:56:16 +0300

    Tests aren't thaaaat bad

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import { useState } from "react";
+
 function App() {
-  return (
-    <div></div>
-  )
+
+  const [checked, setChecked] = useState(false)
+  function handleClick(e) {
+    setChecked(checked => !checked)
+  }
+	return (
+		<div>
+			<h1>Select Pizza Toppings</h1>
+			<input type="checkbox" id="pepperoni" checked={checked} aria-checked={checked} onChange={handleClick} />
+			<label htmlFor="pepperoni">Add pepperoni</label>
+
+			<h2>Your Toppings:</h2>
+			<ul>
+				<li>Cheese</li>
+        {checked? <li>Pepperoni</li>: null}
+			</ul>
+		</div>
+	);
 }
 
 export default App;
